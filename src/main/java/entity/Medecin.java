52de9f4be4fc74bd287f63d47db631daa6697fe8
links <nduwarugirabruno@gmail.com>
2023-01-31 15:02:13 +0100

    first commit

--- src/main/java/entity/Medecin.java
+++ src/main/java/entity/Medecin.java
@@ -1,5 +1,54 @@
 package entity;
 
-public class Medecin {
+public class Medecin extends User {
+    private long idMedecin;
+    private String Poste;
+    private String Specialite;
 
+    public Medecin() {
+        super();
+    }
+
+    public Medecin(String nom, String localite, String profession, long idMedecin, String poste, String specialite) {
+        super(nom, localite, profession);
+        this.idMedecin = idMedecin;
+        Poste = poste;
+        Specialite = specialite;
+    }
+
+    public long getIdMedecin() {
+        return idMedecin;
+    }
+
+    public void setIdMedecin(long idMedecin) {
+        this.idMedecin = idMedecin;
+    }
+
+    public String getPoste() {
+        return Poste;
+    }
+
+    public void setPoste(String poste) {
+        Poste = poste;
+    }
+
+    public String getSpecialite() {
+        return Specialite;
+    }
+
+    public void setSpecialite(String specialite) {
+        Specialite = specialite;
+    }
+
+    @Override
+    public String toString() {
+        return "Medecin [" +
+                "idMedecin= " + idMedecin +
+                ", Poste= " + Poste +
+                ", Specialite= " + Specialite +
+                ", id_Users= " + this.getId_Users() +
+                ", Nom= " + this.getNom() +
+                ", localite= " + this.getLocalite() +
+                ", Profession= " + this.getProfession() + " ]";
+    }
 }
