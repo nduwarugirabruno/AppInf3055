744f2fb2d846fee1e91e9a69d9a7d990cd2c0a89
links <nduwarugirabruno@gmail.com>
2023-01-31 13:47:09 +0100

    first commit

--- src/main/java/controler/ModeleUser.java
+++ src/main/java/controler/ModeleUser.java
@@ -6,18 +6,22 @@ import entity.User;
 
 public class ModeleUser {
 
-	 private String motCle;
-	    List<User> user = new ArrayList<>();
-	    public String getMotCle() {
+	private String motCle;
+	List<User> user = new ArrayList<>();
+
+	public String getMotCle() {
 	    	return motCle;
-	    }
-	    public void setMotCle(String motCle) {
-	    	this.motCle = motCle;
-	    }
-	    public List<User> getUsers() {
-	    	return user;
-	    }
-	    public void setUser(List<User> users) {
-	    	this.user= users;
-	    }
+	}
+
+	public void setMotCle(String motCle) {
+		this.motCle = motCle;
+	}
+
+	public List<User> getUsers() {
+		return user;
+	}
+
+	public void setUser(List<User> users) {
+		this.user= users;
+	}
 }
